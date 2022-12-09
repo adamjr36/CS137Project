1a6f8d09ead94aea17efeec58e34030f88ec7ef8
Adam Rubinstien <adam.rubinstein@tufts.edu>
2022-12-09 14:17:59 -0500

    Added csvs

--- src/data_preprocessing/prep_data.py
+++ src/data_preprocessing/prep_data.py
@@ -6,18 +6,5 @@ data_dir = os.path.join(root, 'cleaned_data')
 
 csvs = os.listdir(data_dir)
 
-def fix_data():
-    for csv in csvs:
-        df = pd.read_csv(os.path.join(data_dir, csv))
-
-        if df.empty:
-            print(csv)
-            assert(0)
-
-        #Change 'home' and 'away' and 'home' with 1 or 0
-        team = csv.split('_')[0]
-        df['Home'] = 1 if (df['Home Team'] == team) else 0
-        df = df.drop(columns=['Home Team', 'Away Team'])
-
 #def train_test_split():
    
