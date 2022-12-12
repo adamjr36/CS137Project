4336b9a3ff26fd3f6851064c37955c07732a2819
WCG OMEN <wenchang.Gao@tufts.edu>
2022-12-11 22:44:33 -0500

    start training

--- src/model/train.py
+++ src/model/train.py
@@ -2,3 +2,41 @@ import torch.nn as nn
 import torch 
 import torch.nn.functional as F 
 import matplotlib.pyplot as plt 
+from torch.utils.data import Dataset, DataLoader
+from dataloader import MyDataset
+from model import BaseModel
+
+# Hyper Parameters
+K = 10
+epochs = 10
+lr = 0.01
+feature_size = 120
+hidden_size1 = 100 
+hidden_size2 = 100
+
+# Preparing train, val, test sets
+# TODO()
+
+# Training
+train_loader = DataLoader(MyDataset('x.csv', 'y.csv'), shuffle=True, batch_size=3)
+model = BaseModel(110, feature_size, hidden_size1, hidden_size2, 3)
+opt = torch.optim.Adam(model.parameters(), lr=lr)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+
+if __name__ == '__main__':
+    for ep in range(epochs):
+        running_loss = 0
+        for i, data in enumerate(train_loader):
+            x, y = data 
+            y = F.one_hot(y, num_classes=3)
+
+            opt.zero_grad()
+            y_hat = model(x)
+            training_loss = loss_fn(y_hat, y.float())
+            running_loss += training_loss.item()
+            training_loss.backward()
+            opt.step()
+        print(f'Epoch {ep}, loss becomes {running_loss}')
+        
+
