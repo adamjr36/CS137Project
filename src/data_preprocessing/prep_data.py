24f9a75e48e20675f466548f8f9a81858249582b
Adam Rubinstien <adam.rubinstein@tufts.edu>
2022-12-13 14:55:36 -0500

    Added validation to train function

--- src/data_preprocessing/prep_data.py
+++ src/data_preprocessing/prep_data.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
 
 root = os.path.dirname(os.path.dirname(os.getcwd()))
 data_dir = os.path.join(root, 'cleaned_data')
 
-csvs = os.listdir(data_dir)
+def tt_split(seed=None, test_size=.2):
+    df_x = pd.read_csv(os.path.join(data_dir, 'x.csv'))
+    df_y = pd.read_csv(os.path.join(data_dir, 'y.csv'))
 
-#def train_test_split():
-   
+    df_x['Win'] = df_y['Win']
+
+    x = x.sample(frac=1, random_state=seed).reset_index()
+    train, test = train_test_split(x, test_size=test_size)
+
+    trainy = train['Win']
+    trainx = train[['Home Team', 'Away Team', 'Date']]
+    
+    testy = test['Win']
+    testx = test[['Home Team', 'Away Team', 'Date']]
+
+    trainx.to_csv(os.path.join(data_dir, 'x_train.csv'))
+    trainy.to_csv(os.path.join(data_dir, 'y_train.csv'))
+    testx.to_csv(os.path.join(data_dir, 'x_test.csv'))
+    testy.to_csv(os.path.join(data_dir, 'y_test.csv'))
+    
+if __name__ == '__main__':
+    tt_split()
+
--- src/model/train.py
+++ src/model/train.py
@@ -18,10 +18,13 @@ hidden_size2 = 256
 # Training
 
 
-def train(train_loader, model, opt, loss_fn, epochs, device):
-    loss = []
+def train(train_loader, val_loader, model, opt, loss_fn, epochs, device):
+    train_loss = []
+    val_loss = []
+    val_acc = []
 
     for ep in range(epochs):
+        #Training
         running_loss = 0
         for i, data in enumerate(train_loader):
             # print(i)
@@ -30,23 +33,38 @@ def train(train_loader, model, opt, loss_fn, epochs, device):
             y = F.one_hot(y, num_classes=3)
 
             opt.zero_grad()
+
             y_hat = model(t1, t2)
             training_loss = loss_fn(y_hat, y.float())
             running_loss += training_loss.item()
             training_loss.backward()
             opt.step()
+
         print(f'Epoch {ep}, loss becomes {running_loss/len(train_loader)}')
-        loss.append(training_loss.item())
-    correct = 0
-    for i, data in enumerate(train_loader):
-        t1, t2, y = data 
-        t1, t2, y = t1.to(device), t2.to(device), y.to(device)
-        y_hat = model(t1, t2)
-        y_hat = torch.argmax(y_hat, axis=1)
-        right = torch.sum(y_hat==y)
-        correct += right
-    print(correct/len(train_loader))
+        train_loss.append(training_loss.item())
+
+        #Validation
+        correct = 0
+        v_loss = 0
+        with torch.no_grad():
+            for i, data in enumerate(val_loader):
+                t1, t2, y = data 
+                t1, t2, y = t1.to(device), t2.to(device), y.to(device)
+                y = F.one_hot(y, num_classes=3)
+
+                y_hat = model(t1, t2)
+                loss = loss_fn(y_hat, y.float())
+                v_loss += loss.item()
+
+                y_hat = torch.argmax(y_hat, axis=1)
+                right = torch.sum(y_hat==y)
+                correct += right
+            print(correct/len(train_loader))
+
+        val_acc.append(correct / len(train_loader))
+        val_loss.append(val_loss.item())
     
-    print(loss)
+    return train_loss, val_loss, val_acc
+
         
 
